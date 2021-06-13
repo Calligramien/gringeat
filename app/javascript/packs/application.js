62b50f8612ad6d670863b8a93ceb8555213bb865
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2021-06-13 23:21:06 +0200

    scanner fixed and accuracy highly improved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -47,64 +47,10 @@ function order_by_occurrence(arr) {
   });
 }
 
-function load_quagga() {
-  if ($('#barcode-scanner').length > 0 && navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
-
-    var last_result = [];
-    if (Quagga.initialized == undefined) {
-      Quagga.onDetected(function (result) {
-        var last_code = result.codeResult.code;
-        last_result.push(last_code);
-
-        if (last_result.length > 20) {
-          const code = order_by_occurrence(last_result)[0];
-          last_result = [];
-          Quagga.stop();
-
-
-
-
-
-
-
-          var xmlHttp = new XMLHttpRequest();
-          xmlHttp.open("GET", `/product/${code}`, true); // false for synchronous request
-          xmlHttp.send(null);
-          console.log(code)
-          window.location.href = `/product/${code}`;
-
-        }
-      });
-    }
-
-    Quagga.init({
-      inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        numOfWorkers: 1,
-        target: document.querySelector('#barcode-scanner')
-      },
-      decoder: {
-        readers: ['ean_reader', 'ean_8_reader', 'code_39_reader', 'code_39_vin_reader', 'codabar_reader', 'upc_reader', 'upc_e_reader']
-      }
-    }, function (err) {
-      if (err) { console.log(err); return }
-      Quagga.initialized = true;
-      Quagga.start();
-    });
-
-  }
-};
-$(document).on('turbolinks:load', load_quagga);
-
-
-
- import { initScanner } from '../plugins/init_scanner';
+import { initScanner } from '../plugins/init_scanner';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2(); 
   initScanner();
 });
-
-
--- app/javascript/plugins/init_scanner.js
+++ app/javascript/plugins/init_scanner.js
@@ -23,19 +23,37 @@ const initScanner = () => {
             Quagga.onDetected(function(result) {
               var last_code = result.codeResult.code;
               last_result.push(last_code);
-              if (last_result.length > 15) {
-              document.querySelector('#panel-body').classList.add('code-detected');
-              }
-              if (last_result.length > 30) {
+              if (last_result.length > 8) {
+                // au bout de 8 photos, il met la barre en vert pour dire "ok mec, j'ai trouvé un truc"
+                document.querySelector('#panel-body').classList.add('code-detected');
+                // sur les 8 photos, je récupère l'occurrence qui a été le plus souvent détecté
                 const code = order_by_occurrence(last_result)[0];
+                // je réinitialise mon tableau pour les prochains scans
                 last_result = [];
-                Quagga.stop();
-                /* fetch()
-                var xmlHttp = new XMLHttpRequest();
-                xmlHttp.open( "GET", `/product/${code}`, true ); // false for synchronous request
-                xmlHttp.send( null ); */
-                console.log(code)
-                window.location.href = `/product/${code}`;
+
+                // log du code produit identifié
+                console.log(code);
+
+                // je fais un check de la page avec le code produit vers OpenFoodFact
+                fetch('https://world.openfoodfacts.org/api/v0/product/' + code + '.json')
+                  // si j'ai un résultat (et spoiler alert, tu en auras toujours un)
+                  .then(res => {
+                    // j'ai un résultat, mais je ne peux pas le traiter 
+                    // je le transforme donc en JSON (comme un tableau)
+                    res.json().then(t1 => { 
+                      // Si mon json me renvoie un statut 1 => c'est que le produit EXISTE REELLEMENT chez Open Food
+                      if(t1.status == 1) {
+                        // on arrête le scanner pour dire "je coupe la caméra"
+                        Quagga.stop();
+                        // on redirige vers la fiche produit
+                        window.location.href = `/products/${t1.code}`; 
+                      }
+                    });
+                    // console.log(res.json().then(t1 => { console.log(t1); }));
+                  })
+                  .catch((out) => {
+                    console.log('Output: ', out);
+                  })
               }
             });
           }
@@ -44,7 +62,7 @@ const initScanner = () => {
             inputStream : {
               name : "Live",
               type : "LiveStream",
-              numOfWorkers: 1,
+              numOfWorkers: 4,
               target: document.querySelector('#barcode-scanner')
             },
             decoder: {
