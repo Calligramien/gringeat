db35931b58988d135b84096e62e4250859ce2666
clementine cheyrou <clemcheyrou@gmail.com>
2021-06-14 17:35:49 +0200

    favourites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_110355) do
+ActiveRecord::Schema.define(version: 2021_06_14_134617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2021_06_14_110355) do
   create_table "product_views", force: :cascade do |t|
     t.integer "views_quantity"
     t.string "code"
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_product_views_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
