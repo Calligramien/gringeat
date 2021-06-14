b33743f190796e038d66f8a8383c523bf533e172
clementine cheyrou <clemcheyrou@gmail.com>
2021-06-14 15:01:40 +0200

    reviews marche de nouveau

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_06_14_105724) do
-
+ActiveRecord::Schema.define(version: 2021_06_14_110355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
