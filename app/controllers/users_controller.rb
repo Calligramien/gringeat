ca898b6931115c796cd87df9e989325c130d1327
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2021-06-15 22:38:34 +0200

    added a view for user profile with redirection for user's profile modification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-
-
-@user.avatar.attached? #=> true/false
-@user.avatar.purge #=> Destroy the avatar
+# @user.avatar.attached? #=> true/false
+# @user.avatar.purge #=> Destroy the avatar
+def index
+    @user = User.find(current_user.id)
+end
 end
