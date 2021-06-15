edf793174b00f87410a64607a3603e4f7bcb1e5c
clementine cheyrou <clemcheyrou@gmail.com>
2021-06-15 17:48:26 +0200

    star les plus recherches

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,19 @@ class PagesController < ApplicationController
     @most_viewed_products = ProductView.order("views_quantity DESC").limit(5).map do |pv|
       Openfoodfacts::Product.get(pv.code, locale: 'fr')
     end
+    
+    @products_datas = {}
+    @most_viewed_products.each do |product|
+      product_reviews = Review.where(product_code: product.code)
+      sumratings = 0
+      if product_reviews.count > 0
+        @products_datas[product.code] = {}
+        product_reviews.each do |review|
+          sumratings += review.ratings
+        end
+        @products_datas[product.code][:average_rating] = sumratings / product_reviews.count
+        @products_datas[product.code][:reviews_count] = product_reviews.count
+      end
+    end
   end
 end
