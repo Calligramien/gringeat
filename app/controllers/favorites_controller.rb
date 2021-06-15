f2fb2b3a14ab63d82ec4012807f3b531dfdeac5c
clementine cheyrou <clemcheyrou@gmail.com>
2021-06-15 14:45:41 +0200

    card + fav

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,11 +4,11 @@ class FavoritesController < ApplicationController
      @favorites = Favorite.all.map do |f|
       Openfoodfacts::Product.get(f.product_code, locale: 'fr')
      end
-    @favorite = Favorite.all
+      @favorite = Favorite.all
   end
   
   def create
-    if Favorite.create(product_code: params[:code], user: current_user)
+    if Favorite.create!(product_code: params[:code], user: current_user)
       redirect_to product_detail_path(params[:code]), notice: 'Product has been favorited'
     else
       redirect_to product_detail_path(params[:code]), alert: 'Something went wrong...'
@@ -19,7 +19,11 @@ class FavoritesController < ApplicationController
     favorite = Favorite.find(params[:id])
     product_code = favorite.product_code
     favorite.destroy
-    redirect_to product_detail_path(product_code), notice: 'Product is no longer in favorites'
+    if request.referer.end_with?("favorites")
+      redirect_to product_favorite_list_path, notice: 'Product is no longer in favorites'
+    else
+      redirect_to product_detail_path(product_code), notice: 'Product is no longer in favorites'
+    end
   end
   
 
