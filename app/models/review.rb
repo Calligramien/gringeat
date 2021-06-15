eb03bcc82853828bd2a9227dca2285c445dcef58
STEPHANE POLETTI <polettistephane@msn.com>
2021-06-15 12:48:57 +0200

    review operationnelle

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   validates :product_code, presence: true
-  validates :ratings, presence: true
-  validates_uniqueness_of :user_id, :scope => [:product_code] 
+  validates :ratings, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  validates_uniqueness_of :user_id, :scope => [:product_code]
+
 end
